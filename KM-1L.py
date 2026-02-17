51870b9d5c36a321abbff271b72c48712441318b
AdminUser <AdminUser@DESKTOP-FK369DJ>
2026-02-17 00:29:30 -0300

    Adicionar arquivos de projeto.

---
+++ KM-1L.py
@@ -0,0 +1,10 @@
+    # Declaração de variáveis com entrada do usuário
+Km = float(input("quilômetros rodados: "))
+    # Número decimal 
+litros = float(input("litros consumidos: "))
+    # Cálculo
+consumo = Km / litros
+    # Nova variável
+
+    # Saída
+print(f"consumo médio: {consumo: .2f} km/1 ")
